63bd49abd95a77363ff66434743a9ec6f92147fb
dheerajdamarla <70621012+dheerajdamarla@users.noreply.github.com>
2021-11-03 23:35:54 -0500

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,4 +1,4 @@
-public class hello {
+public class helloworld {
     public static void main(String[] args) {
         // Print "Hello, World" in the terminal window.
         System.out.println("Hello, World");
